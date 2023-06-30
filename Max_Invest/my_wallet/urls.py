88ed3da16ff467a1af99ab2c965a77484ee5ccc4
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-06-30 01:09:45 -0300

    Update urls.py

--- Max_Invest/my_wallet/urls.py
+++ Max_Invest/my_wallet/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import InvestorList, InvestorDetail, StockList, StockDetail, TransactionList, TransactionDetail
-from .views import get_transactions_by_user, get_transactions_by_month_year, add_transaction, update_transaction, transaction_details, delete_transaction
+from .views import get_transactions_by_user, get_transactions_by_month_year, add_transaction, update_transaction, transaction_details, delete_transaction, PriceAverageAPIView
+
 
 app_name = 'my-wallet'
 
@@ -10,7 +11,7 @@ urlpatterns = [
     path('api/stocks/', StockList.as_view(), name='stock-list'),
     path('api/stocks/<int:pk>/', StockDetail.as_view(), name='stock-detail'),
     path('api/transactions/', TransactionList.as_view(), name='transaction-list'),
-    path('api/transactions/<int:pk>/', TransactionDetail.as_view(), name='transaction-detail'),
+     path('api/transactions/<int:pk>/', TransactionDetail.as_view(), name='transaction-detail'),
     
 
     path('transactions/', get_transactions_by_user, name='transactions'),
@@ -19,4 +20,5 @@ urlpatterns = [
     path('transactions/<int:pk>/update/', update_transaction, name='update_transaction'),
     path('transactions/<int:pk>/', transaction_details, name='transaction_details'),
     path('transactions/<int:pk>/delete', delete_transaction, name='delete_transaction'),
+    path('api/price-average/', PriceAverageAPIView.as_view(), name='price_average'),
 ]
