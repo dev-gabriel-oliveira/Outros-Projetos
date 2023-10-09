33a728223a7cef4b6042e4c17456b797acee2b0d
Gabriel Oliveira <popm4ker@gmail.com>
2023-10-08 21:29:01 -0300

    Dyli v1.1

--- Dyli/src/components/footer/index.jsx
+++ Dyli/src/components/footer/index.jsx
@@ -4,7 +4,7 @@ import './style.css';
 export default function Footer() {
     return(
         <footer>
-            <p>Projeto baseado em redes sociais, feito em ReactJS.</p>
+            <p>Projeto baseado em redes sociais, feito em <a href="">ReactJS</a>.</p>
         </footer>
     )
 }
